1b49bdf6834f48ea5161ba25cd24996b5e82787e
yw3 <yw3@abc.com>
2021-12-02 19:57:13 +0800

    2021120219_commit

---
+++ auto.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+commit_name="`date +%Y%m%d%H`_commit"
+echo "====git auto push start..."
+echo $commit_name
+git add .
+git commit -m $commit_name
+git push origin master
+echo "====git auto push end..."
